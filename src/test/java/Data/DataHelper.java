a55221271c58797b7d087b406bf9248d763e3990
Vadim Kochergin <kochergin1990vadim@gmail.com>
2025-02-26 14:18:46 +0300

    DataHelper added

--- src/test/java/Data/DataHelper.java
+++ src/test/java/Data/DataHelper.java
@@ -1,4 +1,32 @@
 package Data;
 
+import lombok.Value;
+
 public class DataHelper {
+
+    @Value
+    public static class AuthInfo {
+        String username;
+        String password;
+    }
+
+    public static AuthInfo standardUser() {
+        return new AuthInfo("standard_user", "secret_sauce");
+    }
+    public static AuthInfo lockedUser() {
+        return new AuthInfo("locked_out_user", "secret_sauce");
+    }
+    public static AuthInfo problemUser() {
+        return new AuthInfo("problem_user", "secret_sauce");
+    }
+    public static AuthInfo perfomanceUser() {
+        return new AuthInfo("performance_glitch_user", "secret_sauce");
+    }
+    public static AuthInfo errorUser() {
+        return new AuthInfo("error_user", "secret_sauce");
+    }
+    public static AuthInfo visualUser() {
+        return new AuthInfo("visual_user", "secret_sauce");
+    }
+
 }
